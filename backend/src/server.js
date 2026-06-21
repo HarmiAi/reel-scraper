5011d9e29c8ace84bbdc1e846a4e682d14c538f4
harmi123456 <harmipagada4@gmail.com>
2026-06-21 11:31:19 +0530

    add APi health route

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,10 +15,25 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Enable CORS for all requests
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://reel-scraper-silk.vercel.app/'
+    ],
+    credentials: true
+  })
+);
 
 // Parse incoming request JSON bodies
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Reels Backend Running',
+  });
+});
 
 // API healthcheck endpoint
 app.get('/api/health', (req, res) => {
